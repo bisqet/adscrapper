2a01b39ccd3c0c42cea77db0810d642dcbde99d6
Green-Elephant <dflukava@gmail.com>
2018-10-05 21:01:01 +0300

    change GUI

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 const reload = require('require-reload')(require);
 
 let config = reload('../config.js');
-const request = require('request');
+//const request = require('request');
 const fs = require('await-fs');
 const syncFs = require('fs');
 const messageBot = require('../messageBot.js')
@@ -478,6 +478,7 @@ input:focus~.bar:after {
     scrapeLinks.value = \`${config.yad2ResultsURL!==undefined?config.yad2ResultsURL.join('\n'):''}\`;
     unacceptableCities.value = \`${config.cityFilter!==undefined?config.cityFilter.unacceptable.join('\n'):''}\`;
     sqrFilterContainer.value =\`${config.sqrFilter!==undefined?config.sqrFilter:''}\`;
+    unacceptableIds.value = \`${config.unacceptableIDs!==undefined?config.unacceptableIDs.unacceptableIDs.join('\n'):''}\`
     </script>
 </body>
 
@@ -505,7 +506,27 @@ app.post('/changeSettings', (req, res) => {
         return;
     });
 });
+app.post('/banAds', (req, res) => {
 
+    const body = req.body;
+    config.unacceptableIDs.push(body.id);
+    let stringifiedBody = `const config = ${JSON.stringify(config, null, 2)};\nmodule.exports = config;`;
+    fs.writeFile('./config.js', stringifiedBody, 'utf8', (err, data) => {
+        if (err) {
+            log(err);
+            res.send('FAILED TO BAN AD.');
+            messageBot.customMessage({ 'err': 'FAILED TO BAN AD', 'url': 'https://linode.com' });
+            return;
+        }
+        config = reload('../config.js');
+        //messageBot.customMessage({ 'err': 'AD Banned', 'url': 'https://linode.com' });
+
+        log('AD banned');
+
+        res.send('AD banned');
+        return;
+    });
+});
 
 
 app.get('/getLogs', (req, res) => {
