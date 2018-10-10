f3e03dc981b72258f77fcd4af312ca220dbbbbde
Green-Elephant <dflukava@gmail.com>
2018-10-11 02:29:20 +0300

    botfox

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ function indexApp() {
         //await delay(30000); //1m delay.
         await delay(30000);
         const content = await page.content();
-        const cookies = await page.cookies();
+        const cookies = await page.cookies(yad2ResultsURL);
         
         await page.screenshot({ path: publicFolder + 'bancheck.png' });
 
