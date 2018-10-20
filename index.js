e58812a0e792ad5fbfc8c6fbb1f701a3f664e645
Green-Elephant <dflukava@gmail.com>
2018-10-21 02:51:03 +0300

    test

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ function indexApp() {
         fs.writeFileSync('./public/cookies.html', JSON.stringify(cookies, null, 2), 'utf8');
         // check for captcha
         //let captchaExist = await checkForCaptcha(content, page);
-        await delay(150000)
+        await page.waitForNavigation({waitUntil: 'networkidle2'});
         fs.writeFileSync('./public/bancheck.html', content, 'utf8');
         console.info('content wrote to bancheck.html')
 
