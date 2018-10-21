07c37ce89db99ac4e4e08a8c7a550a96df8ed639
Green-Elephant <dflukava@gmail.com>
2018-10-21 15:24:56 +0300

    getADS

--- index.js
+++ index.js
@@ -431,7 +431,7 @@ function indexApp() {
             const browser = await puppeteer.launch({
                 ignoreHTTPSErrors: true,
                 args: ['--no-sandbox',
-                '--proxy-server=185.229.224.61:80'
+                '--proxy-server=147.135.210.114:54566'
                 //`--proxy-server=${WARN_CONFIG.PROXIES[WARN_CONFIG.LAST_PROXY_INDEX].host}:${WARN_CONFIG.PROXIES[WARN_CONFIG.LAST_PROXY_INDEX].port}`
                 ],
                 defaultViewport: {
