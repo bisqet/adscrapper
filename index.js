89393dfd71a98aac1eef2adc49329cd42acfad3f
Green-Elephant <dflukava@gmail.com>
2018-10-21 17:39:37 +0300

    addProxies

--- index.js
+++ index.js
@@ -431,7 +431,7 @@ function indexApp() {
             const browser = await puppeteer.launch({
                 ignoreHTTPSErrors: true,
                 args: ['--no-sandbox',
-                '--proxy-server=95.128.144.1:58193'
+                '--proxy-server=188.170.232.134:43937'
                 //`--proxy-server=${WARN_CONFIG.PROXIES[WARN_CONFIG.LAST_PROXY_INDEX].host}:${WARN_CONFIG.PROXIES[WARN_CONFIG.LAST_PROXY_INDEX].port}`
                 ],
                 defaultViewport: {
